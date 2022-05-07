077391aafb561d64f4b26741522a4e904537da6a
jremi <jzbgdev@gmail.com>
2022-05-06 22:20:57 -0700

    Try catch due to possible ripple timeout error occuring

--- server/helpers/ripple/get-transactions.js
+++ server/helpers/ripple/get-transactions.js
@@ -11,36 +11,41 @@ async function getTransactions(xrpAddress, xrplNetwork) {
         ? process.env.RIPPLE_XRP_WSS_FAUCET_MAINNET
         : process.env.RIPPLE_XRP_WSS_FAUCET_TESTNET,
   });
-  await api.connect();
-  const ledgerVersion = await api.getLedgerVersion();
-  const transactions = await api.getTransactions(xrpAddress, {
-    minLedgerVersion: ledgerVersion - 999999,
-    earliestFirst: false,
-    limit: 20,
-    types: ["payment"],
-  });
-  api.disconnect();
-  const xrphoneTxns = transactions
-    .filter((txn) => {
-      if (txn.specification.memos) {
-        return txn.specification.memos.find((memo) =>
-          memo.data.match("XRPhone")
-        );
-      }
-    })
-    .map((txn) => ({
-      txnId: txn.id,
-      sourceAddress: txn.specification.source.address,
-      destinationAddress: txn.specification.destination.address,
-      timestamp:txn.outcome.timestamp,
-      deliveredAmount: `${txn.outcome.deliveredAmount.value} ${txn.outcome.deliveredAmount.currency}`,
-      memo: txn.specification.memos[0].data,
-      xrplExplorer:
-        xrplNetwork === "MAINNET"
-          ? `https://livenet.xrpl.org/transactions/${txn.id}`
-          : `https://testnet.xrpl.org/transactions/${txn.id}`,
-    }));
-  return xrphoneTxns;
+  try {
+    await api.connect();
+    const ledgerVersion = await api.getLedgerVersion();
+    const transactions = await api.getTransactions(xrpAddress, {
+      minLedgerVersion: ledgerVersion - 999999,
+      earliestFirst: false,
+      limit: 20,
+      types: ["payment"],
+    });
+    api.disconnect();
+    const xrphoneTxns = transactions
+      .filter((txn) => {
+        if (txn.specification.memos) {
+          return txn.specification.memos.find((memo) =>
+            memo.data.match("XRPhone")
+          );
+        }
+      })
+      .map((txn) => ({
+        txnId: txn.id,
+        sourceAddress: txn.specification.source.address,
+        destinationAddress: txn.specification.destination.address,
+        timestamp: txn.outcome.timestamp,
+        deliveredAmount: `${txn.outcome.deliveredAmount.value} ${txn.outcome.deliveredAmount.currency}`,
+        memo: txn.specification.memos[0].data,
+        xrplExplorer:
+          xrplNetwork === "MAINNET"
+            ? `https://livenet.xrpl.org/transactions/${txn.id}`
+            : `https://testnet.xrpl.org/transactions/${txn.id}`,
+      }));
+    return xrphoneTxns;
+  } catch (err) {
+    console.log("get-transactions:catch:err", err.message);
+    return [];
+  }
 }
 
 module.exports = getTransactions;
